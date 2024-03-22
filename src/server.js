43deea03039442d79f7fdef21583660664c45cb5
jericokatong <jericokatong@gmail.com>
2024-03-22 23:22:24 +0800

    restructure with hapi plugin

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const Hapi = require("@hapi/hapi");
+const notes = require("./api/notes");
+const NotesService = require("./services/inMemory/NotesService.js");
+
+const init = async () => {
+  const notesService = new NotesService();
+
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
