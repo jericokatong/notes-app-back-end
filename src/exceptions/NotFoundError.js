1fddd3cfb8f74e016331bcca3cd62f9592d24b59
jericokatong <jericokatong@gmail.com>
2024-04-07 18:52:52 +0800

    FEAT: user registration implemented

--- src/exceptions/NotFoundError.js
+++ src/exceptions/NotFoundError.js
@@ -2,7 +2,7 @@ const ClientError = require('./ClientError');
 
 class NotFoundError extends ClientError {
   constructor(message) {
-    super(message, 400);
+    super(message, 404);
     this.name = 'NotFoundError';
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const ClientError = require('./exceptions/ClientError');
+
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
-const ClientError = require('./exceptions/ClientError');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -19,13 +27,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
