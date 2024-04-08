b8cb7b105db1a2324ac1e31fbca325536e4701ee
jericokatong <jericokatong@gmail.com>
2024-04-08 15:18:43 +0800

    FEAT: authentication implemented

---
+++ src/validator/authentications/index.js
@@ -0,0 +1,30 @@
+const {
+  PostAuthenticationPayloadSchema,
+  PutAuthenticationPayloadSchema,
+  DeleteAuthenticationPayloadSchema,
+} = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const AuthenticationsValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error);
+    }
+  },
+  validatePutAuthenticationPayload: (payload) => {
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error);
+    }
+  },
+  validateDeleteAuthenticationPayload: (payload) => {
+    const validationResult = DeleteAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error);
+    }
+  },
+};
+
+module.exports = AuthenticationsValidator;
